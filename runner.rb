979679c9e17ff251f0321eec35009d45485b702d
Brandon Gray <brandonjoe88@hotmail.com>
2022-11-07 08:20:23 -0700

    feat: extended functionality- all iterations

--- runner.rb
+++ runner.rb
@@ -1,8 +1,49 @@
 require 'benchmark'
 
-time = Benchmark.measure {
-  Dir.chdir('../black_thursday_spec_harness'){
-    system('bundle exec rake spec')
+Dir.chdir('../black_thursday_spec_harness'){
+  time_all = 0
+
+  time = Benchmark.measure {
+    system('bundle exec rspec spec/iteration_1_spec.rb')
   }
+  time_1 = time.real
+  time_all += time.real
+
+  time = Benchmark.measure {
+    system('bundle exec rspec spec/iteration_2_spec.rb')
+  }
+  time_2 = time.real
+  time_all += time.real
+
+  time = Benchmark.measure {
+    system('bundle exec rspec spec/iteration_3_spec.rb')
+  }
+  time_3 = time.real
+  time_all += time.real
+
+  time = Benchmark.measure {
+    system('bundle exec rspec spec/iteration_4_spec.rb')
+  }
+  time_4 = time.real
+  time_all += time.real
+
+  time = Benchmark.measure {
+    system('bundle exec rspec spec/iteration_5_spec.rb')
+  }
+  time_5 = time.real
+  time_all += time.real
+
+  # time = Benchmark.measure {
+  #   system('bundle exec rake spec')
+  # }
+  # puts "Total: #{time.real}"
+  # all = time.real
+
+  puts "Iteration \#1: #{time_1}"
+  puts "Iteration \#2: #{time_2}"
+  puts "Iteration \#3: #{time_3}"
+  puts "Iteration \#4: #{time_4}"
+  puts "Iteration \#5: #{time_5}"
+  puts "Total Iterations: #{time_all}"
+  # puts "Total: #{all}"
 }
-puts time.real
